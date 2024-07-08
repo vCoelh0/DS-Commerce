d16d2f760bb3f5d8e617778562b9cde929d50938
Vinicius <coelhocomercial2k@gmail.com>
2024-07-08 10:18:44 -0300

    Enviando Projeto

---
+++ dscommerce/src/main/java/com/coelhodev/dscommerce/entities/OrderItem.java
@@ -0,0 +1,66 @@
+package com.coelhodev.dscommerce.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name="tb_orderItem")
+public class OrderItem {
+
+	
+	@EmbeddedId
+	private OrderItemPK id = new OrderItemPK();
+	private int quantity;
+	private double price;
+	
+	public OrderItem() {
+		
+	}
+
+	public OrderItem(Order order, Product product, int quantity, double price) {
+		id.setOrder(order);
+		id.setProduct(product);
+		this.quantity = quantity;
+		this.price = price;
+	}
+
+	public Order getOrder() {
+		return id.getOrder();
+	}
+	
+	public void setOrder(Order order) {
+		id.setOrder(order);
+	}
+	
+	
+	
+	public Product getProduct() {
+		return id.getProduct();
+	}
+	
+	public void setProduct(Product prodcut) {
+		id.setProduct(prodcut);
+	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
+	
+	
+}
---
+++ dscommerce/src/main/java/com/coelhodev/dscommerce/entities/OrderStatus.java
@@ -0,0 +1,7 @@
+package com.coelhodev.dscommerce.entities;
+
+public enum OrderStatus {
+	
+	WAITING_PAYMENT, PAID, SHIPPED, DELIVERED, CANCELED;
+	
+}
