f2937405d94e22f67d46b31622eb43b7b5f519c4
vCoelh0 <coelhocomercial2k@gmail.com>
2024-11-28 20:44:57 -0300

    Domain model User ROle, seed

--- dscommerce/src/main/java/com/coelhodev/dscommerce/entities/User.java
+++ dscommerce/src/main/java/com/coelhodev/dscommerce/entities/User.java
@@ -2,21 +2,30 @@ package com.coelhodev.dscommerce.entities;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 
 @Entity
 @Table(name="tb_user")
-public class User {
+public class User implements UserDetails {
 
 	
 	@Id
@@ -33,9 +42,12 @@ public class User {
 	@OneToMany(mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();
 	
-	
-	
-	
+	@ManyToMany
+	@JoinTable(name = "tb_user_role",
+	        joinColumns = @JoinColumn(name = "user_id"),
+	        inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
+
 	
 	
 	public User() {
@@ -105,6 +117,25 @@ public class User {
 	public List<Order> getOrders() {
 		return orders;
 	}
+	
+	
+	
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void addRole(Role role) {
+		roles.add(role);
+	}
+	
+	public boolean hasRole(String roleName) {
+		for (Role role: roles) {
+		if(role.getAuthority().equals(roleName)){
+				return true;
+			}
+		}
+		return false;
+	}
 
 	
 	
@@ -124,6 +155,18 @@ public class User {
 			return false;
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	
