367ae9ebbd1fec180cf2691be57de943b437ad54
vCoelh0 <coelhocomercial2k@gmail.com>
2024-12-04 21:30:49 -0300

    ProductMinDTO

--- dscommerce/src/main/java/com/coelhodev/dscommerce/controllers/ProductController.java
+++ dscommerce/src/main/java/com/coelhodev/dscommerce/controllers/ProductController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.coelhodev.dscommerce.dto.ProductDTO;
+import com.coelhodev.dscommerce.dto.ProductMinDTO;
 import com.coelhodev.dscommerce.services.ProductService;
 
 import jakarta.validation.Valid;
@@ -38,9 +39,9 @@ public class ProductController {
 	}
 	
 	@GetMapping()
-	public ResponseEntity  <Page<ProductDTO>> findAll(
+	public ResponseEntity  <Page<ProductMinDTO>> findAll(
 		@RequestParam (name = "name", defaultValue = "") String name, Pageable pageable) {
-		Page<ProductDTO> dto =  service.findAll(name,pageable);
+		Page<ProductMinDTO> dto =  service.findAll(name,pageable);
 		return ResponseEntity.ok(dto);
 	}
 	
--- dscommerce/src/main/java/com/coelhodev/dscommerce/services/ProductService.java
+++ dscommerce/src/main/java/com/coelhodev/dscommerce/services/ProductService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.coelhodev.dscommerce.dto.ProductDTO;
+import com.coelhodev.dscommerce.dto.ProductMinDTO;
 import com.coelhodev.dscommerce.entities.Product;
 import com.coelhodev.dscommerce.repositories.ProductRepository;
 import com.coelhodev.dscommerce.services.exceptions.DatabaseException;
@@ -32,9 +33,9 @@ public class ProductService {
 	
 	
 	@Transactional(readOnly = true)
-	public Page <ProductDTO> findAll(String name, Pageable pageable) {
+	public Page <ProductMinDTO> findAll(String name, Pageable pageable) {
 		Page <Product> result = repository.searchByName(name, pageable);
-		return result.map(x -> new ProductDTO(x));	
+		return result.map(x -> new ProductMinDTO(x));	
 	}
 	
 	
